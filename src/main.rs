36eea48bdcc633d7ff41e13ef56e2efc418aafdd
mufenqwq <wangpan910@gmail.com>
2024-11-11 16:29:03 +0800

    use tokio to add support to multiple clients

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,38 @@
 #![allow(unused_imports)]
+
+use std::arch::x86_64::_mm256_load_si256;
 use std::net::*;
 use std::io::*;
 use std::ptr::read;
+use tokio::runtime::{Builder, Runtime};
+
 
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
     println!("Logs from your program will appear here!");
 
+    let rt = Runtime::new().unwrap();
 
-    let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
-
-    for stream in listener.incoming() {
-        match stream {
-            Ok(mut stream) => {
-                println!("accepted new connection");
+    rt.block_on(async {
+        let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
+        loop {
+            let (mut socket, _) = listener.accept().unwrap();
+            tokio::spawn(async move {
                 let mut buf = [0; 512];
                 loop {
-                    let read_count = stream.read(&mut buf).unwrap();
-                    if read_count == 0 {
-                        break;
-                    }
-                    stream.write(b"+PONG\r\n").unwrap();
+                    let n = match socket.read(&mut buf) {
+                        Ok(read_count) => {
+                            if read_count == 0 {
+                                return;
+                            }
+                            socket.write(b"+PONG\r\n").unwrap();
+                        }
+                        Err(e ) => {
+                            println!("error: {}", e);
+                        }
+                    };
                 }
-
-
-            }
-            Err(e) => {
-                println!("error: {}", e);
-            }
+            });
         }
-    }
+    });
 }
